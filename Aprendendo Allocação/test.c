c7bef7c909da364e2e2b3372a1a5b562b60d81e5
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-07-10 13:20:19 -0300

    Finish Exerc07.c

---
+++ Aprendendo Allocação/test.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true  1
+#define false 0
+#define PI    3.1415
+
+
+int main() {
+  
+  int i;
+  for(i = 0; i <20; i += 2){
+      printf("\n%d ", i);
+  }
+  
+  return 0;
+}
--- Exerc07.c
+++ Exerc07.c
@@ -1,40 +1,49 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-int fatorial(int n)
-{   
-    int fat, i;
-    fat = 1;
-    for(i = 1; i < n + 1; i++){
-        fat = fat * i;
+#define true 1
+#define false 0
+#define PI 3.1415
+
+// Function to calc fatorial of n
+int fatorial(int n) {
+  int fat, i;
+  fat = 1;
+  for (i = 1; i < n + 1; i++) {
+    fat = fat * i;
+  }
+  return fat;
+}
+
+// Function to calc fatorial of n
+double convert_to_radian(double degree) { return degree * (PI / 180); }
+
+// Function to calc fatorial of n
+double calcSin(double x) {
+  double sin, termo;
+  int i, c = 0;
+  sin = 0;
+  for (i = 1; true; i += 2, c++) {
+    termo = (pow(x, i) / ((double)fatorial(i)));
+    sin += pow((-1.0), c) * termo;
+    if (termo < pow(10, -12)) {
+      break;
     }
-    return fat;
-}
-double calcSin(double x)
-{
-    double sin, ratio;
-    int i;
-    sin = 0;
-    ratio = 0;
-    printf("Ebaaa Carai");
-    for(i = 1; ratio < pow(10, -12); i+=2){
-        sin += ratio*pow(-1, i+1);
-        printf("%f", ratio);
-        ratio = pow(x, i)/fatorial(i);
-    }
-    printf("%f", sin);
-    return sin;
+  }
+  return sin;
 }
 
-int main()
-{   
-   double angulo;
-   printf("Digite o valor para o ƒngulo em radianos: ");
-   scanf("%f", &angulo);
-   double seno = calcSin(angulo);
-   printf("%f", seno);
-   return 0;
+int main() {
+
+  double angulo;
+
+  printf("Digite o valor para o ƒngulo em graus: ");
+  scanf("%lf", &angulo);
+
+  double seno = calcSin(convert_to_radian(angulo));
+
+  printf("\nO seno de %0.2fø ‚ %0.2f.", angulo, seno);
+
+  return 0;
 }
-
-
