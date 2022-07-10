348f9bf52c1076f4b30a7e79a4a8c9109b4d4cec
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-07-09 23:25:05 -0300

    Update and Finish Exerc04.c

--- Exerc04.c
+++ Exerc04.c
@@ -17,7 +17,7 @@ struct data read_data()
     struct data dt;
     char ch, data_string[8];
     int contador = 0;
-    while(contador < 9) {
+    while(contador < 8) {
         ch = getchar();
         if(ch != '\n' && ch != '/'){
             data_string[contador] = ch;
@@ -31,13 +31,27 @@ struct data read_data()
     return dt;
 }
 
-// Data mais recente
-int mais_recente(struct data data_referencia, int tam_vetor, struct data *datas)
+// Convertendo a data para dias 
+int convert_to_days(struct data dt)
 {
-    int i, index_min;
-    for(i = 0;i < tam_vetor; i++){
-        
+    return dt.ano*365 + dt.mes*30 + dt.dia;
+}
+// Data mais recente
+int mais_recente(struct data dt_refer, int tam_vetor, struct data *datas)
+{
+    int i, index_min, menor_dif, data_dias;
+    int dias_refer = convert_to_days(dt_refer);
+    // Datas
+    menor_dif = convert_to_days(datas[0]) - dias_refer;
+    index_min = 0;
+    
+    for(i = 1;i < tam_vetor; i++){
+        if(menor_dif > (convert_to_days(datas[i]) - dias_refer)){
+            menor_dif = convert_to_days(datas[i]) - dias_refer;
+            index_min = 0;
+        }
     }
+    return index_min;
 }
 int main(){
 
@@ -45,18 +59,22 @@ int main(){
     struct data dt_refer;
 
     // Lendo padroes
-    printf("Digite a quantidade de data a serem lidas: ");
-    scanf(" %d", &n);
     printf("Digite data na forma dd/mm/aaaa: ");
     dt_refer = read_data();
+    printf("Digite a quantidade de data a serem lidas: ");
+    scanf(" %d", &n);
+    
     
     // Lendo datas
     struct data dts[n];
     for(i = 0;i < n; i++){
-        printf("Digite data na forma dd/mm/aaaa: ");
+        printf("\nDigite data na forma dd/mm/aaaa: ");
         dts[i] = read_data();
     }
-
+    
+    // Data mais proxima da referenica
+    int index = mais_recente(dt_refer, n, dts);
+    printf("A data mais recente ‚ %d/%d/%d.", dts[index].dia, dts[index].mes, dts[index].ano);
 
     return 0;
 }
