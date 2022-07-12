662885e8d71b7f71ad9b18e10e240b4778d2c23c
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-07-12 15:04:40 -0300

    Refactoring Exerc01.c
    Changing Matricial Manipulaition

--- Exerc01.c
+++ Exerc01.c
@@ -1,154 +1,160 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-void matriz_to_string(int linhas, int colunas, int *matriz)
-{
-    int l;
 
-    for(l = 0; l < linhas*colunas;l++){
-        printf("\t %d \t", *(matriz+l));
-        if (l > 0 && (l+1)%colunas == 0)
-           printf("\n");
+void matriz_to_string(int linhas, int colunas, int mtz[][colunas]) {
+  int l, k;
+
+  for (l = 0; l < linhas; l++) {
+    for ((k = 0); k < colunas; k++) {
+      printf("\t %d \t", mtz[l][k]);
     }
+    printf("\n");
+  }
 }
 
-void sum_elements(int linhas, int colunas, int *matriz)
-{
-    int l;
-    int total = 0;
-    for(l = 0; l < linhas*colunas;l++){
-        total += *(matriz+l);
+void sum_elements(int linhas, int colunas, int mtz[][colunas]) {
+  int l, k;
+  int total = 0;
+  for (l = 0; l < linhas; l++) {
+    for ((k = 0); k < colunas; k++) {
+      total += mtz[l][k];
     }
-    printf("\n A soma dos elementos da matriz ‚ %d.\n",total);
+  }
+  printf("\n A soma dos elementos da matriz ‚ %d.\n", total);
 }
 
-void the_greatest(int linhas, int colunas, int *matriz)
-{
-    int l;
-    int max = *matriz;
-    for(l = 1; l < linhas*colunas;l++){
-        if(max < *(matriz+l)){
-            max = *(matriz+l);
+void the_greatest(int linhas, int colunas, int mtz[][colunas]) {
+  int l, k;
+  int max = mtz[0][0];
+  for (l = 0; l < linhas; l++) {
+    for (k = 0; k < colunas; k++) {
+      if (max < mtz[l][k]) {
+        max = mtz[l][k];
+      }
+    }
+  }
+  printf("\n O maior elemento da matriz ‚ %d.\n", max);
+}
+
+int the_smallest(int linhas, int colunas, int mtz[][colunas]) {
+  int l, k;
+  int min = mtz[0][0];
+  for (l = 0; l < linhas; l++) {
+    for (k = 0; k < colunas; k++) {
+      if (min > mtz[l][k]) {
+        min = mtz[l][k];
+      }
+    }
+  }
+  printf("\n O menor elemento da matriz ‚ %d.\n", min);
+}
+
+void the_greatest_smallest(int linhas, int colunas, int mtz[][colunas]) {
+  the_greatest(linhas, colunas, mtz);
+  the_smallest(linhas, colunas, mtz);
+}
+
+void greater_than_50(int linhas, int colunas, int mtz[][colunas]) {
+  int l, k, contador = 0;
+  for (l = 0; l < linhas; l++) {
+    for (k = 0; k < colunas; k++) {
+      if (mtz[l][k] > 50) {
+        if (contador < 1) {
+          printf("\nElementos maiores que 50: [");
         }
+        printf(" %d,", mtz[l][k]);
+        contador++;
+      }
     }
-    printf("\n O maior elemento da matriz ‚ %d.\n", max);
+  }
+  if (contador > 0) {
+    printf("]\n");
+  } else {
+    printf("\n NÆo h  elementos maiores que 50.");
+  }
 }
 
-int the_smallest(int linhas, int colunas, int *matriz)
+void even_elemnts(int linhas, int colunas, int mtz[][colunas]) 
 {
-    int l;
-    int min = *matriz;
-    for(l = 1; l < linhas*colunas;l++){
-        if(min < *(matriz+l)){
-            min = *(matriz+l);
+  int l, k, contador = 0;
+  for (l = 0; l < linhas; l++) {
+    for (k = 0; k < colunas; k++) {
+      if ((mtz[l][k]%2 == 0)) {
+        if (contador < 1) {
+          printf("\nElementos pares: [");
         }
+        printf(" %d,", mtz[l][k]);
+        contador++;
+      }
     }
-    printf("\n O menor elemento da matriz ‚ %d.\n", min);
+  }
+  if (contador == 0) {
+    printf("\nNÆo h  elementos pares.");
+  } else {
+    printf(" ]");
+  }
 }
 
-void the_greatest_smallest(int linhas, int colunas, int *matriz)
+void count_even_elements(int linhas, int colunas, int mtz[][colunas]) 
 {
-    the_greatest(linhas, colunas, matriz);
-    the_smallest(linhas, colunas, matriz);
+  int l, k, contador = 0;
+  for (l = 0; l < linhas; l++) {
+    for (k = 0; k < colunas; k++) {
+      if ((mtz[l][k]%2 == 0)) {
+        contador++;
+      }
+    }
+  }
+  printf("\nA quantidade de elementos pares ‚: %d", contador);
 }
 
-void greater_than_50(int linhas, int colunas, int *matriz)
-{
-    int l, contador = 0;
-    int total = 0;
-    for(l = 0; l < linhas*colunas;l++){
-        if(*(matriz+l) > 50){
-            if (contador < 1){
-                printf("\nElementos maiores que 50: [");
-            }
-            printf(" %d,", *(matriz+l));
-            contador++;
-        }
+int main() {
+  int i, j;
+
+  // Dimensoes da Matriz
+  printf("Quantidade de linhas da matriz: ");
+  scanf("%d", &i);
+  printf("Quantidade de colunas da matriz: ");
+  scanf("%d", &j);
+
+  // Criando a matriz
+  int mtz[i][j];
+  int l, k;
+  for (l = 0; l < i; l++) {
+    for (k = 0; k < j; k++) {
+      printf("Digite linha %d coluna %d: ", (l + 1), (k + 1));
+      scanf(" %d", &mtz[l][k]);
     }
-    if (contador > 0){
-        printf("]\n");
-    } else {
-        printf("\n NÆo h  elementos maiores que 50.");
-    }
-}
+  }
+  printf("\n\n");
 
-void even_elemnts(int linhas, int colunas, int *matriz)
-{
-    int l, contador = 0;
-    int total = 0;
+  // a. Mostrar cada elemento da matriz;
+  matriz_to_string(i, j, mtz);
 
-    for(l = 0; l < linhas*colunas;l++){
-        if(*(matriz+l) % 2 == 0){
-            if (contador < 1){
-                printf("\nElementos pares: [");
-            }
-            printf(" %d,", *(matriz+l));
-            contador++;
-        }
-    }
-    if (contador == 0){
-        printf("\nNÆo h  elementos pares.");
-    } else {
-        printf(" ]");
-    }
-}
+  // b. Calcular e mostrar a soma dos elementos da matriz;
+  sum_elements(i, j, mtz);
 
-void count_even_elements(int linhas, int colunas, int *matriz)
-{
-    int l, contador = 0;
-    for(l = 0; l < linhas*colunas;l++){
-        if(*(matriz+l) % 2 == 0){
-            contador++;
-        }
-    }
-    printf("\nA quantidade de elementos pares ‚: %d", contador);
-}
-int main()
-{
-    int i, j;
+  // c. Calcular e mostrar o maior elemento da matriz;
+  the_greatest(i, j, mtz);
 
-    // Dimensoes da Matriz
-    printf("Quantidade de linhas da matriz: ");
-    scanf("%d", &i);
-    printf("Quantidade de colunas da matriz: ");
-    scanf("%d", &j);
+  // d. Calcular e mostrar o menor elemento da matriz;
+  the_smallest(i, j, mtz);
 
-    // Criando a matriz
-    int mtz[i][j];
-    int l, k;
-    for (l = 0; l  < i; l++){
-        for (k = 0; k  < j; k++){
-            printf("Digite linha %d coluna %d: ", (l+1), (k+1));
-            scanf(" %d", &mtz[l][k]); 
-        }
-    }
-    
-    // a. Mostrar cada elemento da matriz;
-    matriz_to_string(i, j, *mtz);
-    
-    // b. Calcular e mostrar a soma dos elementos da matriz;
-    sum_elements(i, j, *mtz);
-    
-    // c. Calcular e mostrar o maior elemento da matriz; 
-    the_greatest(i, j, *mtz);
-    
-    // d. Calcular e mostrar o menor elemento da matriz;
-    the_smallest(i, j, *mtz);
+  // e. Calcular e mostrar o maior e o menor elemento da matriz;
+  the_greatest_smallest(i, j, mtz);
 
-    // e. Calcular e mostrar o maior e o menor elemento da matriz;
-    the_greatest_smallest(i, j, *mtz);
-    
-    // f. Exibir cada elemento cujo valor seja maior que 50;
-    greater_than_50(i, j, *mtz);
+  // f. Exibir cada elemento cujo valor seja maior que 50;
+  greater_than_50(i, j, mtz);
 
-    // g. Exibir cada elemento cujo valor seja par;
-    even_elemnts(i, j, *mtz);
-    
-    // g. Exibir cada elemento cujo valor seja par;
-    even_elemnts(i, j, *mtz);
+  // g. Exibir cada elemento cujo valor seja par;
+  even_elemnts(i, j, mtz);
 
-    // h. Calcular e exibir a quantidade de elementos pares da matriz.
-    count_even_elements(i, j, *mtz);
+  // g. Exibir cada elemento cujo valor seja par;
+  even_elemnts(i, j, mtz);
+
+  // h. Calcular e exibir a quantidade de elementos pares da matriz.
+  count_even_elements(i, j, mtz);
 }
